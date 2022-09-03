720643d7c831448276ea287a95d259b769758e48
Marco Sternini <mkarko01@gmail.com>
2022-09-03 16:32:56 +0200

    Initial FXML test

---
+++ src/main/java/hidenseek/Launcher.java
@@ -0,0 +1,22 @@
+package hidenseek;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class Launcher extends Application {
+
+    @Override
+    public final void start(final Stage primaryStage) throws Exception {
+        final Scene root = FXMLLoader.load(ClassLoader.getSystemResource("layouts/MainMenuGui.fxml"));
+        primaryStage.setTitle("Hide'n Seek");
+        primaryStage.setHeight(800);
+        primaryStage.setWidth(600);
+        primaryStage.setScene(root);
+        primaryStage.show();
+    }
+}
