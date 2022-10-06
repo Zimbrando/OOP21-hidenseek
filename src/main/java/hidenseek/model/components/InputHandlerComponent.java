7ca8c0fbde8c972d3f26b399649714052541e571
Marco Sternini <mkarko01@gmail.com>
2022-10-06 19:05:22 +0200

    Added a oneTime callback for input handlers

--- src/main/java/hidenseek/model/components/InputHandlerComponent.java
+++ src/main/java/hidenseek/model/components/InputHandlerComponent.java
@@ -7,7 +7,25 @@ import javafx.scene.input.KeyCode;
 
 public interface InputHandlerComponent extends Component {
     
+    /**
+     * Maps a keyboard key to a callback
+     * @param key, the keycode of the key
+     * @param action, a consumer
+     */
     void mapKeyToAction(KeyCode key, Consumer<Entity> action);
+    
+    /**
+     * Maps a keyboard key to a callback following a 'onTyped' type of behaviour.
+     * The callback is executed once, even if the key has not been released yet.
+     * 
+     * @param key
+     * @param action
+     */
+    void mapKeyToOneTimeAction(KeyCode key, Consumer<Entity> action);
    
+    /**
+     * Executes the callbacks mapped to the relatives keys
+     * @param keysPressed, set of keys currently pressed
+     */
     void computeScheme(Set<KeyCode> keysPressed);
 }
--- src/main/java/hidenseek/model/components/InputHandlerComponentImpl.java
+++ src/main/java/hidenseek/model/components/InputHandlerComponentImpl.java
@@ -1,31 +1,47 @@
 package hidenseek.model.components;
 
+import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Set;
 import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
 import hidenseek.model.Entity;
 import javafx.scene.input.KeyCode;
 
 public class InputHandlerComponentImpl extends AbstractComponent implements InputHandlerComponent {
 
-    Map<KeyCode, Consumer<Entity>> actions;
+    private final Map<KeyCode, Consumer<Entity>> actions;
+    private final Map<KeyCode, Consumer<Entity>> oneTimeActions;
+    private final Set<KeyCode> disabledKeys;
     
     public InputHandlerComponentImpl() {
         this.actions = new LinkedHashMap<KeyCode, Consumer<Entity>>();
+        this.oneTimeActions = new LinkedHashMap<KeyCode, Consumer<Entity>>();
+        this.disabledKeys = new HashSet<KeyCode>();
     }
     
     @Override
     public void mapKeyToAction(KeyCode key, Consumer<Entity> action) {
         this.actions.put(key, action);
     }
+    
+    @Override
+    public void mapKeyToOneTimeAction(KeyCode key, Consumer<Entity> action) {
+        this.oneTimeActions.put(key, action);
+    }
 
     @Override
-    public void computeScheme(Set<KeyCode> keysPressed) {
+    public void computeScheme(Set<KeyCode> keysPressed) {  
+        this.disabledKeys.removeIf(key -> !keysPressed.contains(key));
+      
         keysPressed.forEach(key -> {
-            if (this.actions.containsKey(key)) {
+            if (this.actions.containsKey(key) && !disabledKeys.contains(key)) {
                 this.actions.get(key).accept(this.getOwner().get());
+            } else if (this.oneTimeActions.containsKey(key) && !disabledKeys.contains(key)) {
+                this.oneTimeActions.get(key).accept(this.getOwner().get());
+                this.disabledKeys.add(key);
             }
         });
     }
