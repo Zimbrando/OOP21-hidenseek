182121b028649aef0cdc60abdf78a050a3b95f2e
Marco Sternini <mkarko01@gmail.com>
2022-10-15 11:48:53 +0200

    CollisionHandling Model
    Moved collision handling in GameWorld model

--- src/main/java/hidenseek/controller/GameWorldControllerImpl.java
+++ src/main/java/hidenseek/controller/GameWorldControllerImpl.java
@@ -1,18 +1,12 @@
 package hidenseek.controller;
 
 import java.util.LinkedHashSet;
-import java.util.Optional;
 import java.util.Set;
 
 import hidenseek.controller.entities.EntityController;
-import hidenseek.model.components.CollisionComponent;
-import hidenseek.model.components.Force;
-import hidenseek.model.components.MaterialComponent;
-import hidenseek.model.components.MoveComponent;
-import hidenseek.model.components.PositionComponent;
 import hidenseek.model.worlds.GameWorld;
 import hidenseek.model.worlds.GameWorldImpl;
-import javafx.geometry.Point2D;
+
 
 public final class GameWorldControllerImpl implements GameWorldController {
     
@@ -44,103 +38,11 @@ public final class GameWorldControllerImpl implements GameWorldController {
 
     @Override
     public void update() {
-        //Gestione degli input da tastiera e mouse
-        //      Per ogni entity che ha InputComponent, aggiorniamo la lista di keys, pulsanti del mouse premuti e posizione del cursore
-        //      Otteniamo direction e speed dalla entity
-        
-        //Gestione delle AI
-        //      Per ogni entity che ha AIComponent, otteniamo direction e speed dalla AI
-        
-        //Gestione della posizione degli oggetti
-        //      Calcolare la posizione al frame n+1 delle entity che hanno MovementComponent, in base alla loro direction e speed
-        //      Controlla che la posizione calcolata sia possibile (ovvero non collide con nessun entity che non ha TriggerableComponent)
-        //              Aggiornamento delle eventuale posizione di ogni entity con MovementComponent
-        
-        //Gestione di tutte le collisione eccetto i muri
-        //Questa parte è effettuata logicamente nella sezione precedente, poichè viene eseguita solo per gli oggetti che si sono mossi.
-        //      Trova tutti gli oggetti che si stanno intersecando e manda l'evento intersectionWith(entity) ad entrambi
-        
         // handle inputs
         model.handleInput(this.input.getCurrentPressedKeys());
         
         // update logic
         model.update();
-        
-        // phisics
-        this.entities.stream().forEach(entity -> {
-
-            Optional<PositionComponent> positionComponent = entity.getModel().getComponent(PositionComponent.class);
-            Optional<MoveComponent> moveComponent = entity.getModel().getComponent(MoveComponent.class);
-            Optional<CollisionComponent> collisionComponent = entity.getModel().getComponent(CollisionComponent.class);
-            Optional<MaterialComponent> materialComponent = entity.getModel().getComponent(MaterialComponent.class);
-
-            if(!positionComponent.isPresent()) {
-                return;
-            }
-            
-            if(!moveComponent.isPresent()) {
-                return;
-            }
-
-            //COLLISION DETECT METHOD 2
-            //TODO: write it better with no code repetition
-
-            Point2D resultantOffset = new Point2D(0, 0);
-            for(Force force : moveComponent.get().getForces().toArray(new Force[0])) {
-                if(!collisionComponent.isPresent()) {
-                    return;
-                }
-
-                int forceX = (int)Math.abs(force.getXComponent());
-                int forceXSign = force.getXComponent() < 0 ? -1 : 1;
-                Boolean forceXAccepted = false;
-                int forceY = (int)Math.abs(force.getYComponent());
-                int forceYSign = force.getYComponent() < 0 ? -1 : 1;
-                Boolean forceYAccepted = false;
-                
-                if(!materialComponent.isPresent()) {
-                    resultantOffset = resultantOffset.add(new Point2D(forceX * forceXSign, forceY * forceYSign));
-                    continue;
-                }
-                
-                while(forceX > 0 && !forceXAccepted) {
-                    final int finalForceX = forceX * forceXSign;
-                    if(!this.entities.stream().anyMatch(entity1 -> 
-                                            entity1.getModel().getComponent(MaterialComponent.class).isPresent() && 
-                                            collisionComponent.get().willCollisionWith(entity1.getModel(), new Point2D(finalForceX + forceXSign, 0)))) {
-                        
-                        resultantOffset = resultantOffset.add(new Point2D(finalForceX, 0));
-                        forceXAccepted = true;
-                        break;
-                    }
-                    forceX--;
-                }
-
-                while(forceY > 0 && !forceYAccepted) {
-                    final int finalForceY = forceY * forceYSign;
-                    if(!this.entities.stream().anyMatch(entity1 -> 
-                                            entity1.getModel().getComponent(MaterialComponent.class).isPresent() && 
-                                            collisionComponent.get().willCollisionWith(entity1.getModel(), new Point2D(0, finalForceY + forceXSign)))) {
-                        
-                        resultantOffset = resultantOffset.add(new Point2D(0, finalForceY));
-                        forceYAccepted = true;
-                        break;
-                    }
-                    forceY--;
-                }
-            }
-           
-            
-            positionComponent.get().setPosition(positionComponent.get().getPosition().add(resultantOffset));
-            
-            
-            //this.entities.stream().filter(entity1 -> collisionComponent.get().collisionWith(entity1.getModel())).forEach(entity1 -> {
-        
-                //System.out.println("COLLISION BETWEEN " + entity.toString() + " AND " + entity1.toString());
-            //});
-        });
-
-        System.out.println(this.input.getCurrentPressedKeys());
         
         //Draw game
         view.refresh();
--- src/main/java/hidenseek/model/worlds/GameWorldImpl.java
+++ src/main/java/hidenseek/model/worlds/GameWorldImpl.java
@@ -1,9 +1,16 @@
 package hidenseek.model.worlds;
 
+import java.util.Optional;
 import java.util.Set;
 
+import hidenseek.model.components.CollisionComponent;
+import hidenseek.model.components.Force;
 import hidenseek.model.components.InputHandlerComponent;
+import hidenseek.model.components.MaterialComponent;
+import hidenseek.model.components.MoveComponent;
+import hidenseek.model.components.PositionComponent;
 import hidenseek.model.entities.Entity;
+import javafx.geometry.Point2D;
 import javafx.scene.input.KeyCode;
 
 public class GameWorldImpl extends AbstractEntityWorldImpl implements GameWorld {
@@ -17,6 +24,9 @@ public class GameWorldImpl extends AbstractEntityWorldImpl implements GameWorld 
     
     @Override
     public void update() {
+        
+        this.handleCollisions();
+        
         // ----- handle SenseWorld:
 
         // - Gestione delle AI
@@ -57,5 +67,76 @@ public class GameWorldImpl extends AbstractEntityWorldImpl implements GameWorld 
     public void removeEntity(final Entity e) {
         super.removeEntity(e);
         senseWorld.removeEntity(e);
+    }
+    
+    private void handleCollisions() {
+        // phisics
+        this.world().stream().forEach(entity -> {
+
+            Optional<PositionComponent> positionComponent = entity.getComponent(PositionComponent.class);
+            Optional<MoveComponent> moveComponent = entity.getComponent(MoveComponent.class);
+            Optional<CollisionComponent> collisionComponent = entity.getComponent(CollisionComponent.class);
+            Optional<MaterialComponent> materialComponent = entity.getComponent(MaterialComponent.class);
+
+            if(!positionComponent.isPresent()) {
+                return;
+            }
+            
+            if(!moveComponent.isPresent()) {
+                return;
+            }
+
+            //COLLISION DETECT METHOD 2
+            //TODO: write it better with no code repetition
+
+            Point2D resultantOffset = new Point2D(0, 0);
+            for(Force force : moveComponent.get().getForces().toArray(new Force[0])) {
+                if(!collisionComponent.isPresent()) {
+                    return;
+                }
+
+                int forceX = (int)Math.abs(force.getXComponent());
+                int forceXSign = force.getXComponent() < 0 ? -1 : 1;
+                Boolean forceXAccepted = false;
+                int forceY = (int)Math.abs(force.getYComponent());
+                int forceYSign = force.getYComponent() < 0 ? -1 : 1;
+                Boolean forceYAccepted = false;
+                
+                if(!materialComponent.isPresent()) {
+                    resultantOffset = resultantOffset.add(new Point2D(forceX * forceXSign, forceY * forceYSign));
+                    continue;
+                }
+                
+                while(forceX > 0 && !forceXAccepted) {
+                    final int finalForceX = forceX * forceXSign;
+                    if(!this.world().stream().anyMatch(entity1 -> 
+                                            entity1.getComponent(MaterialComponent.class).isPresent() && 
+                                            collisionComponent.get().willCollisionWith(entity1, new Point2D(finalForceX + forceXSign, 0)))) {
+                        
+                        resultantOffset = resultantOffset.add(new Point2D(finalForceX, 0));
+                        forceXAccepted = true;
+                        break;
+                    }
+                    forceX--;
+                }
+
+                while(forceY > 0 && !forceYAccepted) {
+                    final int finalForceY = forceY * forceYSign;
+                    if(!this.world().stream().anyMatch(entity1 -> 
+                                            entity1.getComponent(MaterialComponent.class).isPresent() && 
+                                            collisionComponent.get().willCollisionWith(entity1, new Point2D(0, finalForceY + forceXSign)))) {
+                        
+                        resultantOffset = resultantOffset.add(new Point2D(0, finalForceY));
+                        forceYAccepted = true;
+                        break;
+                    }
+                    forceY--;
+                }
+            }
+           
+            
+            positionComponent.get().setPosition(positionComponent.get().getPosition().add(resultantOffset));
+            
+        });
     }
 }
